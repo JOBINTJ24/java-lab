2d032a039cbdab8b087962d31da41d1d1999707f
JOBINTJ24 <98311514+JOBINTJ24@users.noreply.github.com>
2022-03-22 19:23:06 +0530

    Add files via upload

---
+++ Comand.java
@@ -0,0 +1,6 @@
+public class Comand{
+	public static void main(String args[]){
+	    for(int i=0;i<args.length;i++){
+		    System.out.println(args[i]);
+}}}			
+		
---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample{
+	public static void main(String args[]){
+	    string c = "jobin";
+		string b = "tj";
+	
+		System.out.println(c+b);
+}}
