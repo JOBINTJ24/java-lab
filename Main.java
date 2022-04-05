1c7cce02c88e627f2240b07f60138537e54d1307
JOBIN T J <98311514+JOBINTJ24@users.noreply.github.com>
2022-04-05 13:02:02 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+ import java.util.*;
+ 
+ public class Main{
+    public static void main(String args[]){
+      String name;
+      System.out.println("enter your name"); 
+      Scanner obj=new Scanner(System.in);
+      name=obj.nextLine();
+      System.out.println(name);
+	}
+}	
